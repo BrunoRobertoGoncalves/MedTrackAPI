72d3795913062eb12909634884ea16766464cd76
Bruno Roberto <goncalves.roberto.bruno@gmail.com>
2025-07-21 16:57:45 -0300

    Reorganize project structure and add TDD with xUnit

--- DTOs/CreateMedicoDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MedTrackAPI.DTOs
-{
-    public class CreateMedicoDTO
-    {
-        public string Nome { get; set; } = string.Empty;
-        public string CRM { get; set; } = string.Empty;
-        public string Especialidade { get; set; } = string.Empty;
-    }
-}
